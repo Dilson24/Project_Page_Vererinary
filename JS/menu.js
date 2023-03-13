260b8b9463016081553b153eba25e3b327f5c240
Dilson24 <dilacn97@hotmail.com>
2023-03-12 21:04:29 -0500

    [NEW]-section two

--- JS/menu.js
+++ JS/menu.js
@@ -1,14 +1,14 @@
-const hamburguer = document.querySelector('.boton_nav')
+const boton_nav = document.querySelector('.boton_nav')
 const menu = document.querySelector('.menu-navegacion')
 
 
-hamburguer.addEventListener('click', ()=>{
+boton_nav.addEventListener('click', ()=>{
     menu.classList.toggle("spread")
 })
 
 window.addEventListener('click', e =>{
     if(menu.classList.contains('spread') 
-        && e.target != menu && e.target != hamburguer){
+        && e.target != menu && e.target != boton_nav){
         console.log('cerrar')
         menu.classList.toggle("spread")
     }
